3ebfed9aa35a6af48a014de940a0185ebea0f48f
Mark <madlyinanger37@gmail.com>
2024-09-13 22:37:50 -0500

    Dynamically creating divs and making choices on click

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -5,8 +5,16 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './game-board.component.html',
-  styleUrl: './game-board.component.scss'
+  styleUrl: './game-board.component.scss',
 })
 export class GameBoardComponent {
+  gameBoardPieces = new Array(9);
+  player1 = new Array(9);
+  player2 = [];
 
+  makeChoice(gridIndex: number) {
+    console.log(gridIndex);
+    this.gameBoardPieces[gridIndex] = 'X';
+    this.player1[gridIndex] = gridIndex;
+  }
 }
