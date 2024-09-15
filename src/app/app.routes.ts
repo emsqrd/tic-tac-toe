12e58d247315b48d48bfd43562e24dfe1fa7d1d6
Mark <madlyinanger37@gmail.com>
2024-09-15 09:07:06 -0500

    Adding route to see if it'll work on github pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,4 +3,5 @@ import { GameBoardComponent } from './game-board/game-board.component';
 
 export const routes: Routes = [
   { path: '', component: GameBoardComponent, pathMatch: 'full' },
+  { path: 'gameboard', component: GameBoardComponent, pathMatch: 'full' },
 ];
