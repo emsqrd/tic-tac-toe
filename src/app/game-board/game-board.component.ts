352ce7bacee13925bca46db1200a5dbb5631ab92
Mark <madlyinanger37@gmail.com>
2024-09-14 22:42:15 -0500

    Basic winner condition

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -10,9 +10,15 @@ import { Component } from '@angular/core';
 export class GameBoardComponent {
   squares = new Array(9);
   currentMove = 1;
+  gameOver: boolean = false;
+  winningPlayer: string | undefined;
 
   makeChoice(gridIndex: number) {
     let isNext = this.currentMove % 2 === 0;
+
+    if (this.squares[gridIndex]) {
+      return;
+    }
 
     if (isNext) {
       this.squares[gridIndex] = 'O';
@@ -20,6 +26,45 @@ export class GameBoardComponent {
       this.squares[gridIndex] = 'X';
     }
 
+    console.log(this.squares);
+    let winner = this.calculateWinner(this.squares);
+
+    if (winner) {
+      this.gameOver = true;
+      this.winningPlayer = this.squares[gridIndex];
+    }
+
     this.currentMove++;
+  }
+
+  resetBoard() {
+    this.squares = new Array(9);
+    this.currentMove = 1;
+    this.gameOver = false;
+  }
+
+  calculateWinner(squares: string[]) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
   }
 }
