0a2d3d663914608664f122d2b567ba9fefe4c418
Mark <madlyinanger37@gmail.com>
2024-09-20 09:12:56 -0500

    Working through actual player mode logic

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Player } from '../models/player';
 
 @Component({
   selector: 'app-game-board',
@@ -8,20 +9,44 @@ import { Component } from '@angular/core';
   styleUrl: './game-board.component.scss',
 })
 export class GameBoardComponent {
-  squares = new Array(9);
-  player1 = new Array(9);
-  player2 = new Array(9);
+  ///TODO: I think I'm on to something here, but need to work through the logic
+  /// The idea is that I have a currentPlayer object that I can do my manipulating with
+  /// but the individual player1 and player2 also get updated when needed
 
-  player1Wins = 0;
-  player2Wins = 0;
+  gameBoard = new Array(9);
+
+  player1: Player = {
+    name: 'Player 1',
+    piece: 'X',
+    wins: 0,
+    isNext: true,
+  };
+
+  player2: Player = {
+    name: 'Player 2',
+    piece: 'O',
+    wins: 0,
+    isNext: false,
+  };
+
   draws = 0;
+
   currentMove = 1;
+
   isResult: boolean = false;
   isDraw: boolean = false;
-  winningPlayer: string | undefined;
+  winningPlayer: Player | null = null;
 
   public get gameOver() {
     return this.isResult || this.isDraw;
+  }
+
+  get currentPlayer(): Player {
+    if (this.player1.isNext) {
+      return this.player1;
+    } else {
+      return this.player2;
+    }
   }
 
   squareClick(square: number) {
@@ -32,40 +57,61 @@ export class GameBoardComponent {
     }
   }
 
-  makeMove(gridIndex: number) {
-    if (this.isResult || this.squares[gridIndex]) {
+  makeMove(square: number) {
+    if (this.isResult || this.gameBoard[square]) {
       return;
     }
 
-    let isNext = this.currentMove % 2 === 0;
+    // let isNext = this.currentMove % 2 === 0;
 
-    if (isNext) {
-      this.squares[gridIndex] = 'O';
-    } else {
-      this.squares[gridIndex] = 'X';
-    }
+    // if (isNext) {
+    //   this.squares[square] = 'O';
+    // } else {
+    //   this.squares[square] = 'X';
+    // }
 
-    let winner = this.calculateWinner(this.squares);
+    this.gameBoard[square] = this.currentPlayer.piece;
+
+    let winner = this.calculateWinner(this.gameBoard);
 
     if (winner) {
       this.isResult = true;
-      this.winningPlayer = this.squares[gridIndex];
 
-      if (this.winningPlayer === 'X') {
-        this.player1Wins++;
-      } else {
-        this.player2Wins++;
-      }
-    } else if (this.currentMove === this.squares.length) {
+      this.setWinner(this.currentPlayer);
+
+      // this.winningPlayer = this.gameBoard[square];
+
+      // if (this.winningPlayer === 'X') {
+      //   this.player1Wins++;
+      // } else {
+      //   this.player2Wins++;
+      // }
+    } else if (this.currentMove === this.gameBoard.length) {
       this.isDraw = true;
       this.draws++;
     }
 
-    this.currentMove++;
+    this.currentPlayer.isNext = this.currentMove % 2 === 0;
+  }
+  setWinner(currentPlayer: Player) {
+    if ((currentPlayer = this.player1)) {
+      this.player1.wins++;
+    } else {
+      this.player2.wins++;
+    }
+  }
+
+  /// TODO: This breaks if we ever have more than two players
+  setCurrentPlayer(): Player {
+    if (this.player1.isNext) {
+      return this.player1;
+    } else {
+      return this.player2;
+    }
   }
 
   resetBoard() {
-    this.squares = new Array(9);
+    this.gameBoard = new Array(9);
     this.currentMove = 1;
     this.isResult = false;
     this.isDraw = false;
---
+++ src/app/models/player.ts
@@ -0,0 +1,6 @@
+export interface Player {
+  name: string;
+  piece: string;
+  wins: number;
+  isNext: boolean;
+}
