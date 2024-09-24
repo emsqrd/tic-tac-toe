2140efcfc91d65dc2c3776a83052b5d8cb225725
Mark <madlyinanger37@gmail.com>
2024-09-24 16:01:54 -0500

    Init swing at unit tests

--- src/app/game-board/game-board.component.spec.ts
+++ src/app/game-board/game-board.component.spec.ts
@@ -1,6 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GameBoardComponent } from './game-board.component';
+import { Square } from '../models/square';
+
+const GAME_BOARD_MOCK: Square[] = [
+  { gamePiece: 'X', isWinner: true },
+  { gamePiece: 'X', isWinner: true },
+  { gamePiece: 'X', isWinner: true },
+  { gamePiece: '', isWinner: false },
+  { gamePiece: '', isWinner: false },
+  { gamePiece: '', isWinner: false },
+  { gamePiece: '', isWinner: false },
+  { gamePiece: '', isWinner: false },
+  { gamePiece: '', isWinner: false },
+];
 
 describe('GameBoardComponent', () => {
   let component: GameBoardComponent;
@@ -8,9 +21,8 @@ describe('GameBoardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GameBoardComponent]
-    })
-    .compileComponents();
+      imports: [GameBoardComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(GameBoardComponent);
     component = fixture.componentInstance;
@@ -19,5 +31,14 @@ describe('GameBoardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should determine that x is the winner if they play the top row', () => {
+    let winner = component.calculateWinner(GAME_BOARD_MOCK);
+    expect(winner).toBe('X');
+  });
+
+  it('should animate the top row if x chooses the top row and wins', () => {
+    let winner = component.calculateWinner(GAME_BOARD_MOCK);
   });
 });
