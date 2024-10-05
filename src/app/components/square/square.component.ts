afa9f96ce5669b5b33fcacdb211ea6e70c5d5412
Mark <madlyinanger37@gmail.com>
2024-10-05 12:29:04 -0500

    Finishing general refactor of square component

--- src/app/components/square/square.component.ts
+++ src/app/components/square/square.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Square } from '../../models/square';
 
 @Component({
   selector: 't3-square',
@@ -9,12 +10,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './square.component.scss',
 })
 export class SquareComponent {
-  @Input() isWinner: boolean = false;
-  @Input() gamePiece = '';
-
-  @Output() squareClicked = new EventEmitter<number>();
-
-  handleSquareClick() {
-    this.squareClicked.emit();
-  }
+  @Input() isWinner: boolean | undefined;
+  @Input() gamePiece: string | undefined;
+  @Input() square: Square = new Square();
 }
