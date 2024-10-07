a8fd515d90782d8428538ef2d4358e072e523747
Mark <madlyinanger37@gmail.com>
2024-10-06 20:27:28 -0500

    Finishing scoring component refactor. Adding tests.

--- src/app/components/scoring/scoring.component.spec.ts
+++ src/app/components/scoring/scoring.component.spec.ts
@@ -20,9 +20,40 @@ describe('ScoringComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should identify the current player', () => {
-    const currentPlayer = fixture.debugElement.nativeElement.querySelector();
+  it('should identify player 1 and their score when they are the current player', () => {
+    const currentPlayer =
+      fixture.debugElement.nativeElement.querySelector('#player1');
+    const currentPlayerScore =
+      fixture.debugElement.nativeElement.querySelector('#player1Score');
 
     component.player1IsCurrent = true;
+    fixture.detectChanges();
+
+    expect(currentPlayer).toHaveClass('current-player');
+    expect(currentPlayerScore).toHaveClass('current-player');
+  });
+
+  it('should identify a draw and the number of draws', () => {
+    const draw = fixture.debugElement.nativeElement.querySelector('#draw');
+    const drawTotal = fixture.debugElement.nativeElement.querySelector('#draw');
+
+    component.isDraw = true;
+    fixture.detectChanges();
+
+    expect(draw).toHaveClass('scoring-result');
+    expect(drawTotal).toHaveClass('scoring-result');
+  });
+
+  it('should identify player 1 as a winner', () => {
+    const winningPlayer =
+      fixture.debugElement.nativeElement.querySelector('#player1');
+    const winningPlayerScore =
+      fixture.debugElement.nativeElement.querySelector('#player1Score');
+
+    component.player1IsWinner = true;
+    fixture.detectChanges();
+
+    expect(winningPlayer).toHaveClass('scoring-result');
+    expect(winningPlayerScore).toHaveClass('scoring-result');
   });
 });
